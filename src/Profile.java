bfbfe549a4d332a3024635ebc1e11aa7215157a5
Markkerg <Markkerg@DESKTOP-PHST0FO>
2023-04-25 10:26:33 +0700

    Thinking Idea......

--- src/Profile.java
+++ src/Profile.java
@@ -24,6 +24,9 @@ public class Profile implements ActionListener {
     private JButton home, pro, setting, save;
     private User user;
     private String usertag = "Invalid username";
+    private ImageIcon imch, imcp, imcs, imch1, imcp1, imcs1;
+    private Font lbfont = new Font("Times New Roman", Font.BOLD, 18);
+    private Font textfont = new Font("Times New Roman", Font.PLAIN, 16);
 public Profile(User user){
     this.user = user;
     fr = new JFrame("Java Touer - Profile");
@@ -34,6 +37,7 @@ public Profile(User user){
     p5 = new JPanel();
     p4.setBackground(Color.white);
             
+    Color blue1 = new Color(76, 177, 221 );
 
     name = new JTextField(this.user.getName());
     last = new JTextField(this.user.getLastName());
@@ -59,10 +63,81 @@ public Profile(User user){
     empt1 =  new JLabel();
     empt2 = new JLabel();
     
-    home = new JButton("home");
-    pro = new JButton("Profile");
-    setting = new JButton("setting");
+
+    
+    nametxt.setFont(lbfont);
+    nametxt.setBounds(120, 150, 100, 100);
+    name.setLocation(120,215);
+    name.setSize(250,30);
+    name.setFont(textfont);
+    warnname.setBounds(120, 200,100,100);
+    
+    lasttxt.setFont(lbfont);
+    lasttxt.setBounds(500,150,100,100);
+    last.setLocation(500,215);
+    last.setSize(250, 30);
+    last.setFont(textfont);
+    warnlast.setBounds(500, 200,100,100);
+    
+    emailtxt.setFont(lbfont);
+    emailtxt.setBounds(120, 240,100,100);
+    email.setLocation(120,305);
+    email.setSize(250, 30);
+    email.setFont(textfont);
+    warnemail.setBounds(120, 290,100,100);
+    
+    teltxt.setFont(lbfont);
+    teltxt.setBounds(500,240,100,100);
+    tel.setLocation(500, 305);
+    tel.setSize(250,30);
+    tel.setFont(textfont);
+    warntel.setBounds(500, 290,200,100);
+    
+    usertxt.setBounds(120,330,100,100);
+    usertxt.setFont(lbfont);
+    username.setLocation(120, 395);
+    username.setSize(250, 30);
+    username.setFont(textfont);
+    warnuser.setBounds(120,380,200,100);
+    
+    passtxt.setBounds(500, 330,100,100);
+    passtxt.setFont(lbfont);
+    pass.setLocation(500,395);
+    pass.setSize(250,30);
+    pass.setFont(textfont);
+    warnpass.setBounds(500,380,200,100);
+    
+    
+    
+           
+    
+    
+    //Button
     save = new JButton("save");
+    imch = new ImageIcon(getClass().getResource("images/home.png"));
+    imcp = new ImageIcon(getClass().getResource("images/profile.png"));
+    imcs = new ImageIcon(getClass().getResource("images/setting.png"));
+    Image sizeh = imch.getImage().getScaledInstance(50, 50, Image.SCALE_SMOOTH);    
+    Image sizep = imcp.getImage().getScaledInstance(50, 50, Image.SCALE_SMOOTH);
+    Image sizes = imcs.getImage().getScaledInstance(50, 50, Image.SCALE_SMOOTH);
+    imch1 = new ImageIcon(sizeh);
+    imcp1 = new ImageIcon(sizep);
+    imcs1 = new ImageIcon(sizes);
+    home = new JButton(imch1);
+    pro = new JButton(imcp1);
+    setting = new JButton(imcs1);
+    
+    save.setBounds(397,480,70,30);
+    save.setFont(lbfont);
+    save.setBackground(Color.orange);
+    save.setForeground(Color.black);
+    
+    
+    home.setBackground(blue1);
+    pro.setBackground(Color.white);
+    
+    
+    setting.setBackground(blue1);
     
     warnname.setForeground(Color.red);
     warnlast.setForeground(Color.red);
@@ -82,39 +157,52 @@ public Profile(User user){
     save.addActionListener(this);
     home.addActionListener(this);
     
+    p2.setBackground(Color.orange);
+    
     p1.setLayout(new BorderLayout());
     p1.add(p2, BorderLayout.WEST);
-    p1.add(p3,BorderLayout.CENTER);
+    p1.add(p4);
     
     p2.setLayout(new GridLayout(6,1));
     p2.add(home); p2.add(pro); p2.add(setting);
     
-    p3.setLayout(new BorderLayout());
-    p3.add(pic, BorderLayout.NORTH);
-    p3.add(p4,BorderLayout.CENTER);
+//    p3.setLayout(new BorderLayout());
+//    p3.add(p4,BorderLayout.CENTER);
     
-    p4.setLayout(new GridLayout(10,2));
+    p4.setLayout(null);
     p4.add(nametxt); p4.add(lasttxt);
     p4.add(name); p4.add(last);
     p4.add(warnname); p4.add(warnlast);
-    p4.add(emailtxt); p4.add(teltxt);
+    p4.add(emailtxt);p4.add(teltxt);
     p4.add(email); p4.add(tel);
     p4.add(warnemail); p4.add(warntel);
     p4.add(usertxt); p4.add(passtxt);
     p4.add(username); p4.add(pass);
     p4.add(warnuser); p4.add(warnpass);
-    p4.add(empt2);p4.add(p5);
+    p4.add(save);
     
-    p5.setLayout(new FlowLayout());
-    p5.add(save);
+//    
+//    p4.add(nametxt); p4.add(lasttxt);
+//    p4.add(name); p4.add(last);
+//    p4.add(warnname); p4.add(warnlast);
+//    p4.add(emailtxt); p4.add(teltxt);
+//    p4.add(email); p4.add(tel);
+//    p4.add(warnemail); p4.add(warntel);
+//    p4.add(usertxt); p4.add(passtxt);
+//    p4.add(username); p4.add(pass);
+//    p4.add(warnuser); p4.add(warnpass);
+//    p4.add(empt2);p4.add(p5);
+//    
+//    p5.setLayout(new FlowLayout());
+//    p5.add(save);
     
     
     //p4.add(warn)
     
+    fr.setResizable(false);
     fr.setContentPane(p1);
     fr.setSize(1000,600);
     fr.setVisible(true);
-   // fr.pack();
     fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     
     
@@ -209,21 +297,35 @@ public Profile(User user){
                 user.setTelNumber(tel.getText());
                 user.setEmail(email.getText());
                 user.setPassword(pass.getText());
-                System.out.println("save");
+                //System.out.println("save");
                 JOptionPane.showConfirmDialog(null, "Update profile success!", "Notification", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE);
+                //test
+//                System.out.println(user.getName());
+//                System.out.println(user.getLastName());
+//                System.out.println(user.getUsername());
+//                System.out.println(user.getPassword());
              }
          
          }
     else{
          warnuser.setVisible(!checkUser(username.getText()));
          if(checkName(name.getText()) && checkName(last.getText()) && checkPass(pass.getText()) && checkTel(tel.getText()) && checkUser(username.getText()) && checkEmail(email.getText())){
+            
+//            
                 user.setName(name.getText());
                 user.setLastName(last.getText());
                 user.setTelNumber(tel.getText());
                 user.setEmail(email.getText());
                 user.setPassword(pass.getText());
-                System.out.println("save");
+               
                 JOptionPane.showConfirmDialog(null, "Update profile success!", "Notification", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE);
+                //test
+//          
+//                System.out.println(user.getName());
+//                System.out.println(user.getLastName());
+//                System.out.println(user.getUsername());
+//                System.out.println(user.getPassword());
+                
                   
          }
           }
@@ -235,6 +337,7 @@ public Profile(User user){
         
     }
     }
+
     public static void main(String[] args) {
         
         User a = new User("Pranpreeya","Mengmungmee", "gyugg21@gmail.com","0959677761","nuneung1122","nuneung112");
