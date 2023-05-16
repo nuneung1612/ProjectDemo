a67485d2ee5fdc419d3c0e71bccef12cea077cad
nuneung1612 <65070131@kmitl.ac.th>
2023-05-16 15:11:57 +0700

    graphic ticketdetail

--- src/TicketDetail.java
+++ src/TicketDetail.java
@@ -15,29 +15,59 @@ import java.util.*;
 import javax.swing.*;
 public class TicketDetail {
     private JFrame fr;
-    private JLabel l1,l2,l3,l4,l5,l6;
+    private JPanel p1,p2;
+    private JLabel l1,l2,l3,l4,l5,l6,l7,eticket;
     private LinkedList <Ticket>ticketData = new LinkedList<Ticket>();
     private LinkedList <Tour> tourData = new LinkedList<Tour>();
+    private ImageIcon barcode;
     
     public TicketDetail(Ticket ticket, User user){
+        Font textfont = new Font("Times New Roman", Font.PLAIN, 16);
+        Font header = new Font("Times New Roman", Font.BOLD, 45);
+        barcode = new ImageIcon(getClass().getResource("images/barcode.png"));
+       
+        Image sizebc = barcode.getImage().getScaledInstance(300, 120, Image.SCALE_SMOOTH);
+        ImageIcon bc1 = new ImageIcon(sizebc);
+ 
+        
         fr = new JFrame("Ticket");
+        p1 = new JPanel(new GridLayout(8,1));
+        p2 = new JPanel(new FlowLayout());
+ 
+        eticket = new JLabel("E-Ticket");
+        eticket.setFont(header);
+        eticket.setForeground(Color.white);
+        p2.setBackground(Color.orange);
         ticketData = FileIO.loadTicketData();
         tourData = FileIO.loadTourData();
         boolean expire = true;
         for (int i = 0; i< tourData.size(); i++){
             if(tourData.get(i).getBusID().equals(ticket.getTour().getBusID())){
-                l1 = new JLabel("TicketId:"+ticket.getTicketID());
-                l2 = new JLabel("Tour:"+ticket.getTour().getBusID()+" - "+ticket.getTour().getType());
-                l3 = new JLabel("Destination: From  "+ticket.getTour().getStart()+"To  "+ticket.getTour().getEnd());
-                l4 = new JLabel("Date:"+ticket.getTour().getDate()+"    Time"+ticket.getTour().getTimeOut()+" ➔ "+ticket.getTour().getTimeArrive());
-                l5 = new JLabel("BookingName: "+user.getName()+"    "+user.getLastName());
-                l6 = new JLabel("Seat: "+ticket.getSeat());
-                fr.setLayout(new GridLayout(7,1));
-                fr.add(l1);     fr.add(l2);     fr.add(l3);     
-                fr.add(l4);     fr.add(l5);     fr.add(l6);
-                fr.setSize(500,600);
+                l1 = new JLabel("      Ticket ID:   "+ticket.getTicketID());
+                l2 = new JLabel("      Bus Type:   "+ticket.getTour().getBusID()+" - "+ticket.getTour().getType());
+                l3 = new JLabel("      Destination:  "+ticket.getTour().getStart()+"  -  "+ticket.getTour().getEnd());
+                l4 = new JLabel("      Date:  "+ticket.getTour().getDate()+"    Time:  "+ticket.getTour().getTimeOut()+" --> "+ticket.getTour().getTimeArrive());
+                l5 = new JLabel("      Booking Name:  "+user.getName()+"     "+user.getLastName());
+                l6 = new JLabel("      Seat:  "+ticket.getSeat());
+                l7 = new JLabel(bc1);
+                l1.setFont(textfont);
+                l2.setFont(textfont);
+                l3.setFont(textfont);
+                l4.setFont(textfont);
+                l5.setFont(textfont);
+                l6.setFont(textfont);
+                
+                p2.add(eticket);
+                p1.add(p2);
+                p1.add(l1);     p1.add(l2);     p1.add(l3);     
+                p1.add(l4);     p1.add(l5);     p1.add(l6);  p1.add(l7);
+                p1.setBackground(Color.white);
+                fr.add(p1);
+                fr.setResizable(false);
+                fr.setSize(400,500);
                 fr.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
                 fr.setVisible(true);
+                
                 expire = false;
             }
         }
